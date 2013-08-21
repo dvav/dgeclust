a8c71329e9f6fe211f8273534006b23a4e4585c4
Dimitris Vavoulis <dimitris.vavoulis@gmail.com>
2013-08-21 11:13:26 +0100

    Added more functions for simulating count data

--- DGEclust/utils/__init__.py
+++ DGEclust/utils/__init__.py
@@ -11,5 +11,5 @@ from getClusterInfo                     import getClusterInfo
 from computeGenePosteriorProbabilities  import computeGenePosteriorProbabilities
 from computeAverageDistanceMatrix       import computeAverageDistanceMatrix
 from computeFittedModel                 import computeFittedModel
-from generateCounts                     import generateUnclusteredCounts, generateClusteredCounts
+from generateCounts                     import generateUnclusteredCounts, generateUnclusteredCounts2, generateClusteredCounts, generateClusteredCounts2
 
--- DGEclust/utils/generateCounts.py
+++ DGEclust/utils/generateCounts.py
@@ -45,6 +45,43 @@ def generateUnclusteredCounts(x, ngenes = 1000, nclasses = 2, nreplicas = 2, nDE
 ################################################################################
 ################################################################################
 
+def generateUnclusteredCounts2(ngenes = 1000, nclasses = 2, nreplicas = 2, nDE = 100, c0 = [7e6, 14e6], w = 1.5, sh1 = 0.28, sc1 = 666., sh2 = 0.85, sc2 = 0.5):      
+    ## sh and sc parameters from Kvam et al. 2012    
+    
+    nclasses -= 1
+    
+    ## parameters for the control condition
+    mu1  = np.random.gamma(shape = sh1, scale = sc1, size = (ngenes,1))
+    phi1 = np.random.gamma(shape = sh2, scale = sc2, size = (ngenes,1))
+    z1   = np.zeros((ngenes,1))
+    c1   = c0[0] + (c0[1] - c0[0]) * np.random.rand(nreplicas)
+
+    ## counts for the control condition
+    y1 = np.random.poisson(np.random.gamma(1. / phi1, c1 * mu1 / mu1.sum() * phi1))
+    
+    ## indicators for DE genes
+    Z   = [ np.zeros((ngenes,1))                                       for i in range(nclasses) ]
+    ZDE = [ np.random.choice(range(ngenes), size=nDE, replace = False) for i in range(nclasses) ]
+    DE  = [ np.random.choice([-1,1], size=(nDE,1))                     for i in range(nclasses) ]
+
+    for z, zde, de in zip(Z,ZDE,DE):        
+        z[zde] = de
+        
+    C   = [ c0[0] + (c0[1] - c0[0]) * np.random.rand(1,nreplicas) for i in range(nclasses) ]        ## library sizes
+    S   = [ w + np.random.exponential(size = (ngenes,1))          for i in range(nclasses) ]        ## regulation sizes
+    PHI = [ phi1                                                  for i in range(nclasses) ]        ## dispersions
+    MU  = [ s**z * mu1                                            for s, z in zip(S,Z) ]            ## means
+    
+    ## draw from the negative binomial
+    Y  = [ np.random.poisson(np.random.gamma(1. / phi, c * mu / mu.sum() * phi)) for c, phi, mu in zip(C, PHI, MU) ]          
+    
+    ## return
+    return np.hstack([y1]+Y), np.hstack([z1]+Z)
+
+
+################################################################################
+################################################################################
+
 def generateClusteredCounts(x0, w0, ngenes = 1000, nclasses = 2, nreplicas = 2, nDE = 100, c0 = [7e6, 14e6]):        
     nclasses -= 1    
     nclusters = len(x0)
@@ -77,6 +114,29 @@ def generateClusteredCounts(x0, w0, ngenes = 1000, nclasses = 2, nreplicas = 2, 
     
     ## return
     return np.hstack([y1]+Y), np.hstack([z1]+Z)
+
+
+################################################################################
+################################################################################
+
+def generateClusteredCounts2(nclusters = 10, ngenes = 1000, nclasses = 2, nreplicas = 2, c0 = [7e6, 14e6], sh1 = 0.28, sc1 = 666., sh2 = 3., sc2 = 1.):        
+    mu0  = np.random.gamma(shape = sh1, scale = sc1, size = nclusters)     
+    phi0 = np.random.gamma(shape = sh2, scale = sc2, size = nclusters)
+    w0   = np.random.rand(nclusters) 
+    w0   = w0 / w0.sum()
+
+    ## class- and gene-specific quantities
+    Z    = [ np.random.choice(range(nclusters), size = (ngenes,1), p = w0) for i in range(nclasses)]   ## cluster indicators
+    C    = [ c0[0] + (c0[1] - c0[0]) * np.random.rand(1,nreplicas)         for i in range(nclasses)]   ## library sizes
+    PHI  = [ phi0[z]                                                       for z    in Z           ]   
+    MU   = [ mu0[z]                                                        for z    in Z           ]
+    
+    ## counts 
+    Y = [ np.random.poisson(np.random.gamma(1. / phi, c * mu / mu.sum() * phi)) for phi, mu, c in zip(PHI,MU,C) ]
+
+    ## return
+    return np.hstack(Y), np.hstack(Z)
+
 
 
 
