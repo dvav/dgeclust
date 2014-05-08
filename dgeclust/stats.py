3d5a9bbe50e078c2e068ce37f7a3cb7368cb9ce2
Dimitris <dimitris.vavoulis@gmail.com>
2014-05-08 14:20:07 +0100

    Slightly changed sampling from the posterior of eta

--- dgeclust/stats.py
+++ dgeclust/stats.py
@@ -109,6 +109,6 @@ def sample_eta(lw, a=2, b=1):
     """Samples the concentration parameter eta given a vector of mixture log-weights"""
 
     ## return
-    return rn.gamma((lw.size - 1) + a, 1 / (b - np.min(lw)))     # 1 / (b - w[-1])
+    return rn.gamma((lw.size - 1) + a, 1 / (b - lw[-1]))
       
 ########################################################################################################################
