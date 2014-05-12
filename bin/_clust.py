2895b534b37ebc10963cc3968519bae79692a896
Dimitris <dimitris.vavoulis@gmail.com>
2014-05-12 18:38:50 +0100

    Fixed small bug in "_clust.py"

--- bin/_clust.py
+++ bin/_clust.py
@@ -67,7 +67,7 @@ if os.path.exists(args.outdir):
         raise Exception("Directory '{0}' already exists!".format(args.outdir))
     else:
         print >> sys.stderr, "Extending previous simulation...".format(args.outdir)
-        state = GibbsState.from_disk(fnames)
+        state = GibbsState.from_file(fnames)
 else:
     os.makedirs(fnames['zz'])
     state = GibbsState.random(data.ngroups, data.nfeatures, model.sample_prior, pars, args.nglobal, args.nlocal)
