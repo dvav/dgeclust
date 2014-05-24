a1d003a4c3519749ad7938e711cfdfede2603227
Dimitris <dimitris.vavoulis@gmail.com>
2014-05-24 18:16:42 +0100

    Fixed Poisson model

--- dgeclust/models/poisson.py
+++ dgeclust/models/poisson.py
@@ -8,7 +8,7 @@ import dgeclust.stats as st
 ########################################################################################################################
 
 
-def _compute_loglik(theta, counts, lib_sizes):
+def _compute_loglik(theta, counts, norm_factors):
     """Computes the log-likelihood of each element of counts for each element of phi and mu"""
 
     ## prepare data
@@ -16,11 +16,11 @@ def _compute_loglik(theta, counts, lib_sizes):
     counts = np.atleast_2d(counts)
     counts = counts[:, :, np.newaxis]
 
-    lib_sizes = np.atleast_2d(lib_sizes).T
-    lib_sizes = lib_sizes[:, :, np.newaxis]
+    norm_factors = np.atleast_2d(norm_factors).T
+    norm_factors = norm_factors[:, :, np.newaxis]
 
     ## return
-    return st.poissonln(counts, lib_sizes * theta)
+    return st.poissonln(counts, norm_factors * theta)
 
 
 ########################################################################################################################
@@ -31,10 +31,10 @@ def compute_loglik(j, data, state):
 
     ## read data
     counts = data.counts[:, data.groups[j]]
-    lib_sizes = data.library_sizes[data.groups[j]]
+    norm_factors = data.norm_factors[data.groups[j]]
 
     ## return
-    return _compute_loglik(state.theta, counts, lib_sizes)
+    return _compute_loglik(state.theta, counts, norm_factors)
 
 ########################################################################################################################
 
@@ -48,18 +48,22 @@ def sample_prior(size, shape, scale):
 ########################################################################################################################
 
 
-def sample_params(theta, shape, _, a0=1, b0=1):
+def sample_params(theta, shape, scale):
     """Samples the shape and scale of the gamma distribution from its posterior, given theta"""
 
     ## sample scale
     s = theta.sum()
+    ls = np.log(theta).sum()
     n = theta.size
 
-    rate = rn.gamma(a0 + n * shape, 1 / (b0 + s))
+    ## sample scale, then sample shape
+    shape = st.sample_gamma_shape(ls, n, shape, scale)
+    scale = st.sample_gamma_scale(s, n, shape)
 
     ## return
-    return shape, 1 / rate
-    
+    return shape, scale
+
+
 ########################################################################################################################
 
 
@@ -68,12 +72,12 @@ def sample_posterior(idx, data, state):
 
     ## fetch all data points that belong to cluster idx
     counts = [data.counts[:, group][zz == idx] for group, zz in zip(data.groups, state.zz)]
-    lib_sizes = [data.library_sizes[group] for group in data.groups]
+    norm_factors = [data.norm_factors[group] for group in data.groups]
 
     s = np.sum([cnts.sum() for cnts in counts])
     n = np.asarray([cnts.size for cnts in counts])
 
-    m = np.sum(lib_sizes * n)
+    m = np.sum(norm_factors * n)
 
     ## parameters
     shape, scale = state.pars
