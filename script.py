fa82cbfe95a68a79197bad1c0ff6543f2f8f27e3
Dimitris <dimitris.vavoulis@gmail.com>
2014-08-18 14:31:01 +0300

    Modified notebooks and script

--- script.py
+++ script.py
@@ -12,9 +12,10 @@ for s in [1]:
             samples = ' '.join(['sample' + str(item) for item in range(1,r+1)*g + np.repeat(range(0,g), r)*10])
             groups = ' ' .join([letter for letter in string.uppercase[:g] for _ in range(r)])
                         
-            cmd = 'clust.py {} -s {} -g {} -o {}'.format(data_file, samples, groups, output_file)
+            cmd = 'clust.py {} -s {} -g {} -o {} -e'.format(data_file, samples, groups, output_file)
             print cmd
             
             p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
             
         p.wait()
+        
